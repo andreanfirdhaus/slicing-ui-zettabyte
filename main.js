cd86283761baf57855156061b02da4352dc0963a
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2025-04-22 22:53:23 +0700

    fix: stop event bubbling from BUY NOW button

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-function showingAlretSucess() {
+function showingAlretSucess(event) {
+    event.stopPropagation();
     alert('Alert Success');
 }
 
-
 function cardIsSelected() {
-    alert('Card Is Selected')
+    alert('Card Is Selected');
+    console.log("The card is selected")
 }
