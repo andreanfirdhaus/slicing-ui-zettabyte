ba41aafae73c417aa8ab3c40904b8455b292950e
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2025-05-03 06:38:53 +0700

    init

--- main.js
+++
@@ -1,9 +0,0 @@
-function showingAlretSucess(event) {
-    event.stopPropagation();
-    alert('Alert Success');
-}
-
-function cardIsSelected() {
-    alert('Card Is Selected');
-    console.log("The card is selected")
-}
